aafa3aafd343792bb7a9b8c9cf0d4c420dacd0a1
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2018-04-18 18:05:38 -0300

    Iniciando o get

--- dist/js/controllers/index-controller.js
+++ dist/js/controllers/index-controller.js
@@ -1,3 +1,11 @@
-app.controller('IndexController', ['$scope', function($scope){
-    $scope.nome = 'Vinicius Nunes';
+app.controller('IndexController', ['$scope', function($scope, $rootScope, $http){
+
+    baseUrl = 'http://187.111.10.182:8998/ideia/core/pessoa/11015472788'
+    
+    $http.get(baseUrl).then(function(response) {
+        $scope.resultado = response.data;
+        
+        console.log(response);
+    });        
+
 }]);
