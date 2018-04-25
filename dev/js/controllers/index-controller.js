991c6bd62c15755a3dcf477c4c0b325945e96293
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2018-04-25 19:16:57 -0300

    validações nos campos nome, email e telefone

--- dev/js/controllers/index-controller.js
+++ dev/js/controllers/index-controller.js
@@ -3,11 +3,15 @@ app.controller('IndexController', ['$scope', '$http', function($scope, $http){
     baseUrl = 'http://187.111.10.182:8998/ideia/core/pessoa/11015472788'
 
     $scope.teste = "vinicius"
+    $scope.validacao_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    $scope.validacao_somente_letras = /^[a-zA-Z ]{1,25}$/;
+    $scope.validacao_somente_numeros = /^[0-9]*$/;
+    $scope.validacao_telefone = /^[0-9]{8,9}$/;
     $scope.pessoa = {
         codigo: "01",
-        cpf: "11015472788",
-        nome_completo: "",
-        email: "vini@",
+        cpf_cnpj: "11015472788",
+        nome_completo: "Vinicius Nunes",
+        email: "exemplo@ideia.com.br",
         telefone: "980346645",
         residencial: "",
         cep: "22733150",
