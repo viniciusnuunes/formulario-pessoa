6459f38888114c8e94d1ff655c3abf1cef543473
Vinicius <viniiciuspereiira@hotmail.com>
2018-05-03 19:14:29 -0300

    Validação de CPF e CNPJ na controller

--- dev/js/controllers/index-controller.js
+++ dev/js/controllers/index-controller.js
@@ -11,11 +11,11 @@ app.controller('IndexController', ['$scope', '$http', function($scope, $http){
         codigo: "P101547278",
         cpf_cnpj: "11015472788",
         nome_completo: "Vinicius Nunes",
-        email: "exemplo@",
-        telefone: "",
+        email: "exemplo@teste.com",
+        telefone: "980346645",
         residencial: "",
         cep: "22733150",
-        rua: "",
+        rua: "A",
         numero_casa: "102",
         bairro: "Tanque",
         complemento: "",        
@@ -29,35 +29,35 @@ app.controller('IndexController', ['$scope', '$http', function($scope, $http){
 
     // função de validação
     $scope.validar = function(obj) {
-        valor = obj.pessoa.cpf_cnpj;
+        valor = (obj.pessoa.cpf_cnpj).replace(/\D/g,'');
         tam = valor.length;
         console.log(valor);
         console.log(tam);
 
         if(!(tam == 11 || tam == 14)) {
-            alert(valor + "não é válido")
+            alert("CPF ou CNPJ inválido")
             return false;
         }
 
         // se for CPF
         if(tam == 11) {
-            if(!validaCPF(valor)) {
+            if(!this.validaCPF(valor)) {
                 alert("O CPF " + valor + " não é válido!");
                 return false;
             }
             alert("CPF válido!")
-            obj.value = maskCPF(valor);
+            obj.pessoa.cpf_cnpj = maskCPF(valor);
             return true;
         }
 
         // se for CNPJ
         if(tam == 14) {
-            if(!validaCNPJ(valor)) {
+            if(!this.validaCNPJ(valor)) {
                 alert("O CNPJ " + valor + " não é válido!");
                 return false
             }
             alert("CNPJ válido!")
-            obj.value = maskCNPJ(valor);
+            obj.pessoa.cpf_cnpj = maskCNPJ(valor);
             return true;
         }
     } // fim do validar
@@ -85,6 +85,36 @@ app.controller('IndexController', ['$scope', '$http', function($scope, $http){
             return false;
         }
         return true;
+    }
+
+    $scope.validaCNPJ =  function(CNPJ) {
+        var a = new Array();
+        var b = new Number;
+        var c = [6,5,4,3,2,9,8,7,6,5,4,3,2];
+        for (i=0; i<12; i++){
+            a[i] = CNPJ.charAt(i);
+            b += a[i] * c[i+1];
+        }
+        if ((x = b % 11) < 2) { a[12] = 0 } else { a[12] = 11-x }
+        b = 0;
+        for (y=0; y<13; y++) {
+            b += (a[y] * c[y]);
+        }
+        if ((x = b % 11) < 2) { a[13] = 0; } else { a[13] = 11-x; }
+        if ((CNPJ.charAt(12) != a[12]) || (CNPJ.charAt(13) != a[13])){
+            return false;
+        }
+        return true;
+    }
+
+    //	função que aplica máscara no CPF
+    function maskCPF(CPF){
+        return CPF.substring(0,3)+"."+CPF.substring(3,6)+"."+CPF.substring(6,9)+"-"+CPF.substring(9,11);
+    }
+
+    //	função que aplica máscara no CNPJ
+    function maskCNPJ(CNPJ){
+        return CNPJ.substring(0,2)+"."+CNPJ.substring(2,5)+"."+CNPJ.substring(5,8)+"/"+CNPJ.substring(8,12)+"-"+CNPJ.substring(12,14);	
     }
     
     // $http.get(baseUrl).then(function successCallback(response) {
